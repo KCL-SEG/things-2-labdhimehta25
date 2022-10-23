b0ed6934225dd9827611d943a55230f08d4acb53
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-23 23:46:04 +0100

    Define ThingForm and add valid and invalid inputs for fields

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,9 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
 # Create your forms here.
+class ThingForm(forms.NOdelForm):
+    class Meta:
+        model = Thingsfiels = {'name', 'description', 'quantity'}
+        widgets = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
